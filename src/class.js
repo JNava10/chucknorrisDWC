8104fdc45a10e36f3a06b35bf216cbd8add221a8
Juan Navarrete <juannr2002@gmail.com>
2023-10-25 18:17:16 +0200

    Peticion de frases por categoria implementada. Pagina de frases implementada.

---
+++ src/class.js
@@ -0,0 +1,10 @@
+export class Joke {
+    constructor(createdAt, url, id, updatedAt, value, categories) {
+        this.createdAt = createdAt;
+        this.url = url;
+        this.id = id;
+        this.updatedAt = updatedAt;
+        this.value = value;
+        this.categories = categories;
+    }
+}
